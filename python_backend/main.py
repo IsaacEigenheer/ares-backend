929a214957107a59a0404eaf4f09f6607fc1eb2b
Isaac Loureiro Eigenheer <eigenheeri@brascabos.intranet>
2024-10-10 13:47:04 -0300

    por favor funciona

--- python_backend/main.py
+++ python_backend/main.py
@@ -173,10 +173,10 @@ def detect_lines_and_save(image, image_name, h, w, config, current_client):
     for rect in non_overlapping_rectangles:
         x1, y1, x2, y2 = rect
         
-        y1 -= (int(h*0.01))
-        y2 += (int(h*0.01))
-        x1 -= 25
-        x2 += 25
+        #y1 -= (int(h*0.01))
+        #y2 += (int(h*0.01))
+        #x1 -= 25
+        #x2 += 25
         cropped_image = image[y1:y2, x1:x2]
         print(cropped_image, flush=True)
         output_path = os.path.join("./cropped_images" , f"{t}{image_name}")##################################################################
