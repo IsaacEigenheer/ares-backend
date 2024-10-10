c60716f2191c0c9fe9913fee689f4af4abc2ef0f
Isaac Loureiro Eigenheer <eigenheeri@brascabos.intranet>
2024-10-10 11:02:50 -0300

    alterando file

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log('Nest is online!!');
-  await app.listen(7001, '0.0.0.0');
+  await app.listen(2001, '0.0.0.0');
 }
 bootstrap();
