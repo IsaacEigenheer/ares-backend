9cbee4e44105bab04f7aea03d7508fd6e1a65d3f
Isaac Loureiro Eigenheer <eigenheeri@brascabos.intranet>
2024-10-14 11:43:24 -0300

    fix

--- python_backend/main.py
+++ python_backend/main.py
@@ -171,16 +171,16 @@ def detect_lines_and_save(image, image_name, h, w, config, current_client):
         x1, y1, x2, y2 = rect
 
         if (y1 - int(h*0.05)) > 0:
-            y1 -= int(h*0.05)
+            y1 = y1 - int(h*0.05)
 
         if(y2 + int(h*0.05)) < h:
-            y2 +=  int(h*0.05)
+            y2 = y2 + int(h*0.05)
 
-        if (x1 - 5) > 0:
-            x1 -= 15
+        if (x1 - 25) > 0:
+            x1 = x1 - 25
         
-        if (x1 + 15) < w:
-            x2 += 15
+        if (x2 + 25) < w:
+            x2 = x2 + 25
 
         cropped_image = image[y1:y2, x1:x2]
         output_path = os.path.join("./cropped_images" , f"{t}{image_name}")##################################################################
