0ae60e8c4094d936d2ef5c1a60bf15b209ea2b33
Isaac Loureiro Eigenheer <eigenheeri@brascabos.intranet>
2025-07-23 14:40:08 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log('Nest is online!!');
+  app.enableCors();
   await app.listen(2001, '0.0.0.0');
 }
 bootstrap();
